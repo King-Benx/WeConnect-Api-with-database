8a4f2b5ea030625c979f2e6f77ad198d81c3ad95
KingBenx <kingbenx17@gmail.com>
2018-03-23 03:14:13 +0300

    setting up demo sqlite database [Fixes #155327560]

---
+++ weconnect-api-with-database/config.py
@@ -0,0 +1,29 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'very hard key'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV-DATABASE') or 'sqlite:////'+os.path.join(basedir,'dev-data.sqlite')
+
+class ProductionConfig(Config):
+    pass
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
