591f0aa8b2b241494e6ee1b05919b068e3f155bc
KingBenx <kingbenx17@gmail.com>
2018-03-23 03:43:04 +0300

    creation of error handler 400 and 500 in main/errors.py  [Fixes #155327560]

---
+++ weconnect-api-with-database/app/main/errors.py
@@ -0,0 +1,9 @@
+from . import main
+
+@main.app_errorhandler(404)
+def page_not_known(e):
+    return 'unknown page',404
+
+@main.app_errorhandler(500)
+def internal_server_error(e):
+    return 'internal server error', 500
