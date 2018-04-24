e8625d1977c224d7a0d1712e154564b2a051aa49
KingBenx <kingbenx17@gmail.com>
2018-04-24 07:23:40 +0300

    refactored authentication.py for lean code [Fixes #155327560]

--- app/api_v_1/authentication.py
+++ app/api_v_1/authentication.py
@@ -38,43 +38,30 @@ def token_required(f):
 def login():
     """Login registered users into systems and assign a token"""
     data = request.get_json(force=True)
-    if len(data.keys()) == 2:
-        email = data['email']
-        password = data['password']
-        if check_validity_of_mail(email) != None:
-            if User.query.filter_by(email=email).count() == 0:
-                return make_response(
-                    "Could not verify! wrong email, Try again " +
-                    str(url_for('api.login', _external=True))), 401
-            else:
-                user = User.query.filter_by(email=email).first()
-                password_validity = user.check_password(password)
-                if not password_validity:
-                    return make_response(
-                        "Could not verify! password incorrect, Try again " +
-                        str(url_for('api.login', _external=True))), 401
-                else:
-                    token = jwt.encode(
-                        {
-                            'id':
-                            user.id,
-                            'exp':
-                            datetime.datetime.utcnow() +
-                            datetime.timedelta(minutes=20)
-                        },
-                        Config.SECRET_KEY)
-                    return make_json_reply({
-                        'user_status':
-                        'Successfully Logged in',
-                        'username':
-                        user.username,
-                        'email':
-                        email,
-                        'token':
-                        token.decode('UTF-8')
-                    }), 200
-        else:
-            return make_json_reply('Invalid Email, Try again'), 400
-    else:
+    if len(data.keys()) != 2:
         return make_response("Unknown user, register now or try to Login again"
                              + str(url_for('api.login', _external=True))), 404
+    email = data['email']
+    password = data['password']
+    if check_validity_of_mail(email) == None:
+        return make_json_reply('Invalid Email, Try again'), 400
+    if User.query.filter_by(email=email).count() == 0:
+        return make_response("Could not verify! wrong email, Try again " +
+                             str(url_for('api.login', _external=True))), 401
+    user = User.query.filter_by(email=email).first()
+    password_validity = user.check_password(password)
+    if not password_validity:
+        return make_response("Could not verify! password incorrect, Try again "
+                             + str(url_for('api.login', _external=True))), 401
+    token = jwt.encode(
+        {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=20)
+        },
+        Config.SECRET_KEY)
+    return make_json_reply({
+        'user_status': 'Successfully Logged in',
+        'username': user.username,
+        'email': email,
+        'token': token.decode('UTF-8')
+    }), 200
