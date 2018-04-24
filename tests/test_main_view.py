167667c8483354443b120ba5a9a066d51ad75eef
KingBenx <kingbenx17@gmail.com>
2018-04-24 17:32:52 +0300

    test views.py in main [Fixes #155327560]

---
+++ tests/test_main_view.py
@@ -0,0 +1,8 @@
+import unittest
+from flask import url_for
+from app import create_app, db
+from app.main import main
+
+class TestMain(unittest.TestCase):
+    def test_index_page(self):
+        self.assertTrue(url_for('main.index',_external=True), 200)
