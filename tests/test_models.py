026e43e58f679a8a3bd6301647cb37e5c54e6531
KingBenx <kingbenx17@gmail.com>
2018-04-26 07:47:00 +0300

    tests for blacklistedtokens in models [Fixes #155327560]

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,14 +1,16 @@
 import unittest
-from app.models import User, Business, Review
+from app.models import User, Business, Review, BlackListedTokens
 
 
 class TestModels(unittest.TestCase):
     """Tests the model representations in the models"""
 
-    def test_user_repr_(self):
+    def test_user_repr(self):
         user = User(
             username='janedoe', password='pass', email='janedoe@mail.com')
         self.assertTrue(user.__repr__() == "User 'janedoe'")
+        with self.assertRaises(AttributeError): user.password
+
 
     def test_business_repr(self):
         business = Business(
@@ -21,3 +23,8 @@ class TestModels(unittest.TestCase):
     def test_review_repr_(self):
         review = Review(user_id=1, business_id=1, review='business 1 review')
         self.assertTrue(review.__repr__() == "Review 'business 1 review'")
+
+    def test_blacklistedtokens_repr(self):
+        blacklistedtokens = BlackListedTokens(token='abcdefghijklmnopqrs')
+        self.assertTrue(blacklistedtokens.__repr__() ==
+                        "Blacklisted Token : 'abcdefghijklmnopqrs'")
